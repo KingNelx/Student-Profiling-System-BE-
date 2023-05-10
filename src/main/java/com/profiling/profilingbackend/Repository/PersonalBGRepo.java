3737cc562d05fc50ffff31f763090095c6722731
KingNelx <tapiajonel606@gmail.com>
2023-05-10 09:55:07 +0800

    added personal bg repo

--- src/main/java/com/profiling/profilingbackend/Repository/PersonalBGRepo.java
+++ src/main/java/com/profiling/profilingbackend/Repository/PersonalBGRepo.java
@@ -1,5 +1,9 @@
 package com.profiling.profilingbackend.Repository;
 
-public interface PersonalBGRepo {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.profiling.profilingbackend.Model.PersonalBG;
+
+public interface PersonalBGRepo extends MongoRepository <PersonalBG, String>{
     
 }
