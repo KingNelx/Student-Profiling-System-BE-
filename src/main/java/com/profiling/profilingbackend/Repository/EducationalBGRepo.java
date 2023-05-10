60e8cc80ba06065da84e59e23c1790d6b02282e7
KingNelx <tapiajonel606@gmail.com>
2023-05-10 09:53:57 +0800

    added family bg repo

--- src/main/java/com/profiling/profilingbackend/Repository/EducationalBGRepo.java
+++ src/main/java/com/profiling/profilingbackend/Repository/EducationalBGRepo.java
@@ -2,6 +2,8 @@ package com.profiling.profilingbackend.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.profiling.profilingbackend.Model.EducationalBG;
+
 public interface EducationalBGRepo extends MongoRepository <EducationalBG, String> {
     
 }
--- src/main/java/com/profiling/profilingbackend/Repository/FamilyBGRepo.java
+++ src/main/java/com/profiling/profilingbackend/Repository/FamilyBGRepo.java
@@ -1,5 +1,9 @@
 package com.profiling.profilingbackend.Repository;
 
-public interface FamilyBGRepo {
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.profiling.profilingbackend.Model.FamilyBG;
+
+public interface FamilyBGRepo extends MongoRepository <FamilyBG, String>{
     
 }
