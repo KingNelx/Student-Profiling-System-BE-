2d323799439a50ed9e43d4e5d0497e4505a296ad
KingNelx <tapiajonel606@gmail.com>
2023-06-12 14:35:08 +0800

    added configuration for admin

--- src/main/java/com/profiling/profilingbackend/Repository/AdminRepo.java
+++ src/main/java/com/profiling/profilingbackend/Repository/AdminRepo.java
@@ -2,6 +2,12 @@ package com.profiling.profilingbackend.Repository;
 
 import com.profiling.profilingbackend.Model.Admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
 
 public interface AdminRepo extends MongoRepository <Admin, String> {
+    Optional <Admin> findByEmail(String email);
+    Optional <Admin> findByUsername(String username);
+    Admin findByEmailAndUserNameAndPassword(String email, String username, String password);
+
+
 }
