2154f9eceabc357e1fef18bf5921e66f4a58b58b
KingNelx <tapiajonel606@gmail.com>
2023-08-07 22:30:32 +0800

    reset all

--- src/main/java/com/profiling/profilingbackend/Repository/FamilyBGRepo.java
+++
@@ -1,7 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Model.FamilyBG;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FamilyBGRepo extends MongoRepository <FamilyBG, String> {
-}
--- src/main/java/com/profiling/profilingbackend/Repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-public interface StudentRepo extends MongoRepository <Student, String> {
-    List <Student> findByGender(String gender);
-    List <Student> findByCourse(String course);
-}
