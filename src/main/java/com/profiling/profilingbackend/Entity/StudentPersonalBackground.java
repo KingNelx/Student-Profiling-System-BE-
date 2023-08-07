2149253c41a93357b0412ce02b2565ac77e422bf
KingNelx <tapiajonel606@gmail.com>
2023-08-07 23:34:00 +0800

    added student repo and entity

---
+++ src/main/java/com/profiling/profilingbackend/Entity/StudentPersonalBackground.java
@@ -0,0 +1,4 @@
+package com.profiling.profilingbackend.Entity;
+
+public class StudentPersonalBackground {
+}
---
+++ src/main/java/com/profiling/profilingbackend/Repository/StudentEducationalRepo.java
@@ -0,0 +1,4 @@
+package com.profiling.profilingbackend.Repository;
+
+public interface StudentEducationalRepo {
+}
