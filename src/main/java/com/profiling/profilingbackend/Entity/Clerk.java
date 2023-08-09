5198e7c87ba21972d40c0a8a807a8a7a10e62d55
KingNelx <tapiajonel606@gmail.com>
2023-08-09 09:48:19 +0800

    added clerk entity

--- src/main/java/com/profiling/profilingbackend/Entity/Clerk.java
+++ src/main/java/com/profiling/profilingbackend/Entity/Clerk.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Entity
@@ -16,5 +17,14 @@ public class Clerk {
 
     @Id
     private @Getter @Setter String id;
-
+    @Column(nullable = false)
+    private @Getter @Setter String firstName;
+    @Column(nullable = false)
+    private @Getter @Setter String lastName;
+    @Column(nullable = false)
+    private @Getter @Setter String email;
+    @Column(nullable = false)
+    private @Getter @Setter String userName;
+    @Column(nullable = false)
+    private @Getter @Setter String password;
 }
