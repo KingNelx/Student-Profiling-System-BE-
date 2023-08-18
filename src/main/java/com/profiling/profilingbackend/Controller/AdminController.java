11daba8df78197760a832d2fa8561d85541b332b
KingNelx <tapiajonel606@gmail.com>
2023-08-18 09:48:34 +0800

    added cors config

--- src/main/java/com/profiling/profilingbackend/Controller/AdminController.java
+++ src/main/java/com/profiling/profilingbackend/Controller/AdminController.java
@@ -6,6 +6,7 @@ import com.profiling.profilingbackend.Service.AdminService;
 import com.profiling.profilingbackend.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AdminController {
 
     @Autowired
