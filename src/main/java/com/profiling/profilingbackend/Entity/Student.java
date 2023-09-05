b035844ab46f11f226adf3dc3fd9dfe6ae290684
KingNelx <tapiajonel606@gmail.com>
2023-09-05 23:34:56 +0800

    STUDENT ENTITY CREATED

---
+++ src/main/java/com/profiling/profilingbackend/Entity/Student.java
@@ -0,0 +1,49 @@
+package com.profiling.profilingbackend.Entity;
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
+
+@Document(collection = "Student")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Student {
+
+    @Id
+    private @Getter @Setter String id;
+
+    @Column(nullable=false)
+    private @Getter @Setter String studentID;
+
+    @Column(nullable=false)
+    private @Getter @Setter String firstName;
+
+    @Column(nullable = false)
+    private @Getter @Setter String lastName;
+
+    @Column(nullable=false)
+    private @Getter @Setter String middleInitial;
+
+    @Column(nullable=false)
+    private @Getter @Setter LocalDate dateOfBirth;
+
+    @Column(nullable = false)
+    private @Getter @Setter String gender;
+
+    @Column(nullable=false)
+    private @Getter @Setter String contactNumber;
+
+    @Column(nullable=false)
+    private @Getter @Setter String emailAddress;
+
+    @Column(nullable = false)
+    private @Getter @Setter String address;
+}
