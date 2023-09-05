a5c20e8e9b7706da9d23bb8caaf67643f9ff2bef
KingNelx <tapiajonel606@gmail.com>
2023-09-05 21:24:16 +0800

    reset

--- src/main/java/com/profiling/profilingbackend/Repository/StudentEducationalRepo.java
+++
@@ -1,7 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Entity.StudentEducationalBackground;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentEducationalRepo extends MongoRepository <StudentEducationalBackground, String> {
-}
--- src/main/java/com/profiling/profilingbackend/Repository/StudentFamilyRepo.java
+++
@@ -1,7 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Entity.StudentFamilyBackground;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentFamilyRepo extends MongoRepository <StudentFamilyBackground, String> {
-}
