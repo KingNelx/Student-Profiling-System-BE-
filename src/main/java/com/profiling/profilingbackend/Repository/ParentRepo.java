a806def69148a5d67b9e9a040ab4d00e3cc3cd84
KingNelx <tapiajonel606@gmail.com>
2023-11-07 08:53:19 +0800

    reset

--- src/main/java/com/profiling/profilingbackend/Repository/ParentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Entity.Parents;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ParentRepo extends MongoRepository <Parents, String> {
-}
