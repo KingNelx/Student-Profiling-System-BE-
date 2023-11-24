7df0b6f7aa53e1d835e6784d82369d59013f30ab
KingNelx <tapiajonel606@gmail.com>
2023-11-24 13:18:26 +0800

    reset

--- src/main/java/com/profiling/profilingbackend/Repository/CourseRepo.java
+++
@@ -1,7 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Entity.Course;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CourseRepo extends MongoRepository <Course, String> {
-}
--- src/main/java/com/profiling/profilingbackend/Repository/SubjectRepo.java
+++
@@ -1,7 +0,0 @@
-package com.profiling.profilingbackend.Repository;
-
-import com.profiling.profilingbackend.Entity.Subjects;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface SubjectRepo extends MongoRepository <Subjects, String> {
-}
