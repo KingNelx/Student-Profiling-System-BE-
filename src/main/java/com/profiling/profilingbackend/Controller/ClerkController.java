cfbe3a8a93b07afb524624907235dac617f4bb4a
KingNelx <tapiajonel606@gmail.com>
2023-11-29 13:05:18 +0800

    clerk controller finish

---
+++ src/main/java/com/profiling/profilingbackend/Controller/ClerkController.java
@@ -0,0 +1,54 @@
+package com.profiling.profilingbackend.Controller;
+
+import com.profiling.profilingbackend.Entity.Clerk;
+import com.profiling.profilingbackend.Service.ClerkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
+@RestController
+@RequestMapping("/clerk")
+public class ClerkController {
+
+    @Autowired
+    private ClerkService clerkService;
+
+    @PostMapping("/create-account")
+    public ResponseEntity <String> createAccount(@RequestBody Clerk clerk){
+        return clerkService.createAccount(clerk);
+    }
+
+    @GetMapping("/queryAll")
+    public List <Clerk> queryClerks(){
+        return clerkService.queryClerks();
+    }
+
+    @GetMapping("query-clerk-id/{id}")
+    public Optional <Clerk> queryClerkById(@PathVariable String id){
+        return clerkService.queryClerkById(id);
+    }
+
+    @PutMapping("/update-data")
+    public ResponseEntity <String> updateClerkData(@PathVariable String id, @RequestBody Clerk clerk){
+        return clerkService.updateClerkData(id, clerk);
+    }
+
+    @DeleteMapping("/delete-data")
+    public ResponseEntity <String> deleteClerkData(@PathVariable String id){
+        return clerkService.deleteClerkData(id);
+    }
+
+    @GetMapping("/sign-in")
+    public ResponseEntity <String> logInAsClerk(@RequestParam String email, @RequestParam String userName, @RequestParam String password){
+        return clerkService.logInAsClerk(email, userName, password);
+    }
+}
