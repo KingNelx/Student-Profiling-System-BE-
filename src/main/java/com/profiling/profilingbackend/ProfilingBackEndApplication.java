b85a40e8ad5f7271dac47ecccc3e9efdaa10c641
KingNelx <tapiajonel606@gmail.com>
2024-03-14 14:29:52 +0800

    reset

--- src/main/java/com/profiling/profilingbackend/ProfilingBackEndApplication.java
+++ src/main/java/com/profiling/profilingbackend/ProfilingBackEndApplication.java
@@ -8,4 +8,26 @@ public class ProfilingBackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProfilingBackEndApplication.class, args);
 	}
+
+	/*
+	 * public class PasswordProtectionProgram {
+	 * public static void main(String[] args) {
+	 * HashMap<String, String> credentials = new HashMap<>();
+	 * credentials.put("user1", "password123");
+	 * credentials.put("user2", "abc123");
+	 * Scanner scanner = new Scanner(System.in);
+	 * System.out.print("Enter your username: ");
+	 * String username = scanner.nextLine();
+	 * System.out.print("Enter your password: ");
+	 * String enteredPassword = scanner.nextLine();
+	 * String correctPassword = credentials.get(username);
+	 * if (correctPassword != null && enteredPassword.equals(correctPassword)) {
+	 * System.out.println("Access granted! You have entered the correct password.");
+	 * } else {
+	 * System.out.println("Access denied! Incorrect username or password.");
+	 * }
+	 * scanner.close();
+	 * }
+	 * }
+	 */
 }
