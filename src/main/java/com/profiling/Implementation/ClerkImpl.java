7ae02441bdc388f78658f8198fbe6b7827593e65
KingNelx <tapiajonel606@gmail.com>
2024-03-14 14:44:53 +0800

    created implementation folder

---
+++ src/main/java/com/profiling/Implementation/ClerkImpl.java
@@ -0,0 +1,12 @@
+package com.profiling.Implementation;
+
+import org.springframework.stereotype.Service;
+
+import com.profiling.profilingbackend.Service.ClerkService;
+import java.util.Optional;
+import org.springframework.http.RequestEntity;
+
+@Service
+public class ClerkImpl {
+
+}
